778cf4c121d1ee98bd0c49c59aa5e076501d6b0c
Lucifer <a2771234@gamil.com>
2022-07-14 16:09:38 +0800

    BRANCH2 BY MYSELF

--- src/main/java/Luciffer.java
+++ src/main/java/Luciffer.java
@@ -1,6 +1,6 @@
 
 public class Luciffer {
 public static void main(String[] args) {
-	System.out.println("做不出來要靠ott!!!!!!!!!!!! OOOOOOOOOOOOOOOo");
+	System.out.println("做不出來要靠ott!!!!!!!!!!!! OOOOOOOOOOOOOOOoo");
 }
 }
